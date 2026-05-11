47110687a491a66f5a9dc703648586d46fb18e6f
Pavan Kumar <pavan.kumar@ens.enterprises>
2026-05-11 12:52:27 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import express from "express";
 import dotenv from "dotenv";
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constant";
@@ -6,7 +7,18 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
+
+const app = express();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(" MongoDB connection Faild !!! ", error);
+  });
+
 /*
 import express from "express";
 const app = express()
