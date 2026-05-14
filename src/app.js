888856a5f9def03b58dbfdf9ff9fe6151d597a99
Pavan Kumar <pavan.kumar@ens.enterprises>
2026-05-14 11:08:42 +0530

    fix asycHander file error return and check api  /register

--- src/app.js
+++ src/app.js
@@ -15,4 +15,9 @@ app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
 export { app };
