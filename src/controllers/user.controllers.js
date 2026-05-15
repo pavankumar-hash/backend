971204064d27ba02a7e429745b3e4cd7992a8921
Pavan Kumar <pavan.kumar@ens.enterprises>
2026-05-15 18:19:26 +0530

    coverImageLocalPath error resolve

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -21,7 +21,18 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // console.log(req.files);
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
@@ -41,6 +52,7 @@ const registerUser = asyncHandler(async (req, res) => {
     password,
     userName,
   });
+  console.log("User registered successfully");
   const createUser = await User.findById(user._id).select(
     "-password -refreshToken"
   );
